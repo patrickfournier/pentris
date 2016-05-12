93f59125f3bb7175cfbfe94642d974bf73506451
Patrick Fournier <patrick@whiskyechobravo.com>
2016-05-11 23:05:18 -0400

    Such progress!!!

--- brico.js
+++ brico.js
@@ -1,5 +1,16 @@
 document.addEventListener('DOMContentLoaded', function() {
 
+  /* TODO:
+
+   - how to play
+   - review key assignment
+   - share (twitter, FB, ...)
+   - high score
+   - dynamic background image
+   - fix sounds
+   - animate line removal
+   */
+
   var brico = brico || {};
 
   /*
@@ -29,14 +40,12 @@ document.addEventListener('DOMContentLoaded', function() {
 
   /* Color */
   brico.Color = function(c, s) {
-    this.r = brico.Palette[c][s][0];
-    this.g = brico.Palette[c][s][1];
-    this.b = brico.Palette[c][s][2];
-    this.a = brico.Palette[c][s][3];
+    var color = brico.Palette[c][s];
+    this.colorString = "rgba(" + color[0].toString() + "," + color[1].toString() + "," + color[2].toString() + "," + color[3].toString() + ")";
   };
 
   brico.Color.prototype.toString = function() {
-    return "rgba(" + this.r.toString() + "," + this.g.toString() + "," + this.b.toString() + "," + this.a.toString() + ")";
+    return this.colorString;
   };
 
   /* Shape */
@@ -53,6 +62,22 @@ document.addEventListener('DOMContentLoaded', function() {
     for (var i = 0; i < shape.length; i++) {
       this.def[shape[i]] = true;
     }
+
+    var minX = shapeWidth;
+    var maxX = 0;
+    var minY = shapeHeight;
+    var maxY = 0;
+    var squares = this.getOccupiedSquares();
+    for (var i = 0; i < squares.length; i++) {
+      minX = Math.min(minX, squares[i][0]);
+      maxX = Math.max(maxX, squares[i][0]);
+      minY = Math.min(minY, squares[i][1]);
+      maxY = Math.max(maxY, squares[i][1]);
+    }
+    this.bboxLeft = minX;
+    this.bboxTop = minY;
+    this.bboxWidth = maxX - minX + 1;
+    this.bboxHeight = maxY - minY + 1;
   };
 
   brico.Shape.prototype.rotationLookupTable = {
@@ -72,33 +97,17 @@ document.addEventListener('DOMContentLoaded', function() {
     this.rotation = (this.rotation + delta) % 360;
   };
 
-  brico.Shape.prototype.draw = function(grid) {
-    var color1 = this.color1.toString();
-    var color2 = this.color2.toString();
-    var rlut = this.rotationLookupTable[this.rotation];
-
-    grid.drawSquarePrepare();
-    for (var i = 0; i < this.def.length; i++) {
-      var j = rlut[i];
-      if (this.def[j]) {
-        var x = (this.x + (i % this.width));
-        var y = (this.y + Math.floor(i / this.width));
-        grid.drawSquare(x, y, color1, color2);
-      }
-    }
-  };
-
   brico.Shape.prototype.getOccupiedSquares = function() {
     return this.getOccupiedSquaresAfterMove(0, 0, undefined);
   };
 
-  brico.Shape.prototype.getOccupiedSquaresAfterMove = function(dx, dy, rotation) {
+  brico.Shape.prototype.getOccupiedSquaresAfterMove = function(dx, dy, clockwiseRotation) {
     var occupiedSquares = [];
     var r = this.rotation;
-    if (rotation === true) {
+    if (clockwiseRotation === true) {
       r = (this.rotation + 90) % 360;
     }
-    else if (rotation === false) {
+    else if (clockwiseRotation === false) {
       r = (this.rotation + 270) % 360;
     }
 
@@ -145,24 +154,125 @@ document.addEventListener('DOMContentLoaded', function() {
     return shape;
   };
 
-  brico.Grid = function(interval) {
-    var canvas = document.getElementById('brico');
+  brico.ShapeFactory.randomShape = function() {
+    var r = Math.random();
+    var shapeId = Math.floor(r * this.shapes.length);
+    return this.newShape(shapeId);
+  };
+
+  /* GridView */
+  brico.GridView = function(canvasId, grid) {
+    var canvas = document.getElementById(canvasId);
     this.ctx = canvas.getContext('2d');
 
-    this.pixelWidth = canvas.height/2;
-    this.pixelHeight = 2*this.pixelWidth;
-    this.width = 13;
-    this.height = 26;
-    this.x = (canvas.width - this.pixelWidth) / 2;
-    this.y = 0;
+    this.grid = grid;
 
-    this.xs = this.pixelWidth / this.width;
-    this.ys = this.pixelHeight / this.height;
+    this.x = 0;
+    this.y = 0;
+    this.pixelWidth = canvas.width;
+    this.pixelHeight = canvas.height;
+
+    this.xs = this.pixelWidth / this.grid.width;
+    this.ys = this.pixelHeight / this.grid.height;
     this.insetX = Math.floor(this.xs/6);
     this.insetY = Math.floor(this.ys/6);
 
+    this.lastGridUpdateId = grid.updateId - 1;
+  };
+
+  brico.GridView.prototype.drawSquarePrepare = function() {
+    this.ctx.strokeStyle = "rgba(30,30,30,1)";
+    this.ctx.lineWidth = 2;
+  };
+
+  brico.GridView.prototype.drawSquare = function(x, y, color1Str, color2Str) {
+    x = x * this.xs + this.x;
+    y = y * this.ys + this.y;
+    this.ctx.fillStyle = color1Str;
+    this.ctx.fillRect(x, y, this.xs, this.ys);
+    this.ctx.fillStyle = color2Str;
+    this.ctx.fillRect(x+this.insetX, y+this.insetY, this.xs-2*this.insetX, this.ys-2*this.insetY);
+    this.ctx.strokeRect(x, y, this.xs, this.ys);
+  };
+
+  brico.GridView.prototype.draw = function() {
+    if (this.lastGridUpdateId != grid.updateId) {
+      this.lastGridUpdateId = grid.updateId;
+
+      this.ctx.clearRect(this.x, this.y, this.pixelWidth, this.pixelHeight);
+
+      this.drawSquarePrepare();
+      for (var i = 0; i < this.grid.gridData.length; i++) {
+        if (this.grid.gridData[i]) {
+          var x = i % this.grid.width;
+          var y = Math.floor(i / this.grid.width);
+          var c1 = this.grid.gridData[i][0].toString();
+          var c2 = this.grid.gridData[i][1].toString();
+          this.drawSquare(x, y, c1, c2);
+        }
+      }
+
+      if (this.grid.currentShape) {
+        var c1 = this.grid.currentShape.color1.toString();
+        var c2 = this.grid.currentShape.color2.toString();
+        var squares = this.grid.currentShape.getOccupiedSquares();
+        for (var i = 0; i < squares.length; i++) {
+          this.drawSquare(squares[i][0], squares[i][1], c1, c2);
+        }
+      }
+    }
+  };
+
+  brico.GridView.prototype.animate = function() {
+    this.draw();
+    window.requestAnimationFrame(this.animate.bind(this));
+  };
+
+  /* Shape Preview */
+  brico.ShapePreviewView = function(canvasId, grid) {
+    brico.GridView.call(this, canvasId, grid);
+
+    this.xs = this.pixelWidth / 6;
+    this.ys = this.pixelHeight / 6;
+    this.insetX = Math.floor(this.xs/6);
+    this.insetY = Math.floor(this.ys/6);
+
+    this.lastShapeDrawn = this.grid.nextShape;
+  };
+  brico.ShapePreviewView.prototype = Object.create(brico.GridView.prototype);
+
+  brico.ShapePreviewView.prototype.draw = function() {
+    if (this.lastShapeDrawn != this.grid.nextShape) {
+      this.lastShapeDrawn = this.grid.nextShape;
+
+      this.ctx.clearRect(this.x, this.y, this.pixelWidth, this.pixelHeight);
+
+      this.drawSquarePrepare();
+
+      if (this.grid.nextShape) {
+        var padX = (6 - this.grid.nextShape.bboxWidth) / 2. - this.grid.nextShape.bboxLeft;
+        var padY = (6 - this.grid.nextShape.bboxHeight) / 2. - this.grid.nextShape.bboxTop;
+        var c1 = this.grid.nextShape.color1.toString();
+        var c2 = this.grid.nextShape.color2.toString();
+        var squares = this.grid.nextShape.getOccupiedSquares();
+        for (var i = 0; i < squares.length; i++) {
+          this.drawSquare(squares[i][0] + padX, squares[i][1] + padY, c1, c2);
+        }
+      }
+    }
+  };
+
+  /* Grid */
+  brico.Grid = function(width, height, interval) {
+    this.width = width;
+    this.height = height;
+
+    this.nextShape = brico.ShapeFactory.randomShape();
     this.currentShape = undefined;
+
     this.gridData = new Array(this.width * this.height);
+
+    this.updateId = 0;
 
     var self = this;
     this.periodicTask = window.setInterval(function() {
@@ -178,44 +288,12 @@ document.addEventListener('DOMContentLoaded', function() {
         this.gridData[j] = [this.currentShape.color1, this.currentShape.color2];
       }
       this.currentShape = undefined;
-    }
-  };
 
-  brico.Grid.prototype.drawSquarePrepare = function() {
-    grid.ctx.strokeStyle = "rgba(60,60,60,1)";
-    grid.ctx.lineWidth = 2;
-  };
+      var event = document.createEvent('Event');
+      event.initEvent('brico-bake', true, true);
+      document.dispatchEvent(event);
 
-  brico.Grid.prototype.drawSquare = function(x, y, color1Str, color2Str) {
-    x = x * this.xs + this.x;
-    y = y * this.ys + this.y;
-    grid.ctx.fillStyle = color1Str;
-    grid.ctx.fillRect(x, y, this.xs, this.ys);
-    grid.ctx.fillStyle = color2Str;
-    grid.ctx.fillRect(x+this.insetX, y+this.insetY, this.xs-2*this.insetX, this.ys-2*this.insetY);
-    grid.ctx.strokeRect(x, y, this.xs, this.ys);
-  };
-
-  brico.Grid.prototype.draw = function() {
-    grid.ctx.clearRect(this.x, this.y, this.pixelWidth, this.pixelHeight);
-
-    grid.ctx.strokeStyle = "rgba(60,60,60,1)";
-    grid.ctx.lineWidth = 2;
-    grid.ctx.strokeRect(this.x, this.y, this.pixelWidth, this.pixelHeight);
-
-    this.drawSquarePrepare();
-    for (var i = 0; i < this.gridData.length; i++) {
-      if (this.gridData[i]) {
-        var x = i % this.width;
-        var y = Math.floor(i / this.width);
-        var c1 = this.gridData[i][0].toString();
-        var c2 = this.gridData[i][1].toString();
-        this.drawSquare(x, y, c1, c2);
-      }
-    }
-
-    if (this.currentShape) {
-      this.currentShape.draw(this);
+      this.updateId++;
     }
   };
 
@@ -237,6 +315,7 @@ document.addEventListener('DOMContentLoaded', function() {
       var shapeSquares = this.currentShape.getOccupiedSquaresAfterMove(dx, dy, undefined);
       if (!this.detectCollision(shapeSquares)) {
         this.currentShape.move(dx, dy);
+        this.updateId++;
         return true;
       }
     }
@@ -248,6 +327,7 @@ document.addEventListener('DOMContentLoaded', function() {
       var shapeSquares = this.currentShape.getOccupiedSquaresAfterMove(0, 0, clockwise);
       if (!this.detectCollision(shapeSquares)) {
         this.currentShape.rotate(clockwise);
+        this.updateId++;
         return true;
       }
     }
@@ -266,6 +346,14 @@ document.addEventListener('DOMContentLoaded', function() {
       }
       if (dy > 1) {
         this.currentShape.move(0, dy - 1);
+
+        var event = document.createEvent('Event');
+        event.dy = dy - 1;
+        event.initEvent('brico-drop', true, true);
+        document.dispatchEvent(event);
+
+        this.updateId++;
+
         return true;
       }
     }
@@ -275,7 +363,7 @@ document.addEventListener('DOMContentLoaded', function() {
   brico.Grid.prototype.removeFullLines = function() {
     var fullLines = [];
 
-    for (var j = 0; j < this.height; j++) {
+    for (var j = this.height - 1; j >= 0; j--) {
       var isFull = true;
       for (var i = 0; i < this.width; i++) {
         if (!this.gridData[j * this.width + i]) {
@@ -288,14 +376,21 @@ document.addEventListener('DOMContentLoaded', function() {
       }
     }
 
-    for (var i = 0; i < fullLines.length; i++) {
-      var j = fullLines[i];
-      this.gridData.splice(j*this.width, this.width);
-    }
-    var newLines = new Array(fullLines.length * this.width);
-    this.gridData = newLines.concat(this.gridData);
+    if (fullLines.length > 0) {
+      for (var i = 0; i < fullLines.length; i++) {
+        var j = fullLines[i];
+        this.gridData.splice(j*this.width, this.width);
+      }
+      var newLines = new Array(fullLines.length * this.width);
+      this.gridData = newLines.concat(this.gridData);
 
-    return fullLines.length;
+      var event = document.createEvent('Event');
+      event.lines = fullLines.length;
+      event.initEvent('brico-lines', true, true);
+      document.dispatchEvent(event);
+
+      this.updateId++;
+    }
   };
 
   brico.Grid.prototype.tick = function() {
@@ -305,50 +400,123 @@ document.addEventListener('DOMContentLoaded', function() {
       // delete lines
       this.removeFullLines();
       // create new shape.
-      var r = Math.random();
-      var shapeId = Math.floor(r * brico.ShapeFactory.shapes.length);
-      this.currentShape = brico.ShapeFactory.newShape(shapeId);
+      this.currentShape = this.nextShape;
       if (!this.tryMove((this.width - 5)/2, 0)) {
         // game over
+        this.nextShape = undefined;
         this.currentShape = undefined;
         window.clearInterval(this.periodicTask);
+
+        var event = document.createEvent('Event');
+        event.initEvent('brico-gameover', true, true);
+        document.dispatchEvent(event);
+      }
+      else {
+        this.nextShape = brico.ShapeFactory.randomShape();
       }
     }
-    this.draw();
   };
 
-  var grid = new brico.Grid(500);
+  /* Game */
+  brico.Game = function(grid) {
+    this.score = 0;
+    this.lineCompleted = 0;
+    this.level = 1;
 
-  document.onkeypress = function(e) {
-    e = e || window.event;
-    var charCode = (typeof e.which == "number") ? e.which : e.keyCode;
-    if (charCode) {
-      switch (String.fromCharCode(charCode)) {
-      case '4':
-        grid.tryMove(-1, 0);
-        grid.draw();
-        break;
-      case '5':
+    this.scoreElem = document.getElementById('brico-score');
+    this.levelElem = document.getElementById('brico-level');
+
+    this.dropSound = [new Audio('sounds/drop.mp3'), new Audio('sounds/drop.mp3'), new Audio('sounds/drop.mp3')];
+    this.linesSound = new Audio('sounds/magic_wand.mp3');
+    this.fourLinesSound = new Audio('sounds/cheers.mp3');
+    this.gameOverSound = new Audio('sounds/sad_trombone.mp3');
+
+    document.addEventListener('brico-bake', brico.Game.prototype.bakeEventListener.bind(this));
+    document.addEventListener('brico-lines', brico.Game.prototype.fullLinesEventListener.bind(this));
+    document.addEventListener('brico-drop', brico.Game.prototype.dropEventListener.bind(this));
+    document.addEventListener('brico-gameover', brico.Game.prototype.gameOverEventListener.bind(this));
+
+    document.onkeypress = function(e) {
+      e = e || window.event;
+      var charCode = (typeof e.which == "number") ? e.which : e.keyCode;
+      if (charCode) {
+        switch (String.fromCharCode(charCode)) {
+        case 'j':
+          grid.tryMove(-1, 0);
+          break;
+        case 'k':
         grid.tryRotate(false);
-        grid.draw();
-        break;
-      case '6':
-        grid.tryMove(1, 0);
-        grid.draw();
-        break;
-      case '8':
-        grid.tryRotate(true);
-        grid.draw();
-        break;
-      case '0':
-        grid.drop();
-        grid.draw();
-        break;
-      case '.':
-        grid.tryMove(0, 1);
-        grid.draw();
-        break;
+          break;
+        case 'l':
+          grid.tryMove(1, 0);
+          break;
+        case 'i':
+          grid.tryRotate(true);
+          break;
+        case ' ':
+          grid.drop();
+          grid.bake();
+          break;
+      case 'h':
+          grid.tryMove(0, 1);
+          break;
+        }
       }
+    };
+  };
+
+  brico.Game.prototype.bakeEventListener = function(e) {
+    this.score += 5;
+    this.scoreElem.innerHTML = this.score;
+  };
+
+  brico.Game.prototype.fullLinesEventListener = function(e) {
+    this.linesSound.currentTime = 0;
+    this.linesSound.play();
+
+    this.score += 10 * e.lines;
+    if (e.lines == 4) {
+      this.score += 100;
+      this.fourLinesSound.currentTime = 0;
+      this.fourLinesSound.play();
+    }
+
+    this.score += 10 * e.lines;
+    if (e.lines == 4) {
+      this.score += 100;
+    }
+    this.scoreElem.innerHTML = this.score;
+    this.lineCompleted += e.lines;
+    while (this.lineCompleted > 10) {
+      this.level++;
+      this.levelElem.innerHTML = this.level;
+      this.lineCompleted -= 10;
     }
   };
+
+  brico.Game.prototype.dropEventListener = function(e) {
+    var i = 0;
+    while ((i < this.dropSound.length) && !this.dropSound[i].paused) {
+      i++;
+    }
+    if (i == 3) {
+      i = 0;
+    }
+    this.dropSound[i].currentTime = 0;
+    this.dropSound[i].play();
+  };
+
+  brico.Game.prototype.gameOverEventListener = function(e) {
+    this.gameOverSound.currentTime = 0;
+    this.gameOverSound.play();
+    document.getElementById('brico-gameover').style.display = "block";
+  };
+
+  var grid = new brico.Grid(13, 26, 500);
+  var gridView = new brico.GridView('brico-board', grid);
+  var shapePreview = new brico.ShapePreviewView('brico-preview', grid);
+  var game = new brico.Game(grid);
+
+  gridView.animate();
+  shapePreview.animate();
 }, false);
